c0d8b278f30ed716535a39e6acb20b3aa15ab135
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-01-12 16:25:45 +0300

    Задача про боксеров

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var res1 = dog + 4;
-        var res2 = cat + 4;
-        var res3 = paper + 4;
-        System.out.println(res1);
-        System.out.println(res2);
-        System.out.println(res3);
-        var res4 = dog - 3.5;
-        var res5 = cat - 1.6;
-        var res6 = paper - 7639;
-        System.out.println(res4);
-        System.out.println(res5);
-        System.out.println(res6);
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var totalWeight = boxer1 + boxer2;
+        System.out.println("Общий вес боксеров: " + totalWeight + "кг");
+
+        var weightDifference = boxer2 % boxer1;
+        System.out.println("Разница в весе боксеров составляет: " + weightDifference + "кг");
     }
 }
