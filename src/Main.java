f1a45f93d35a987e54d7880e7d7e40a696543df1
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-01-12 15:06:31 +0300

    Задачи 1-3. Сложение и вычетание. Вариант 2

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,18 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-        var res1 = dog + 4;
-        var res2 = cat + 4;
-        var res3 = paper + 4;
-        System.out.println(res1);
-        System.out.println(res2);
-        System.out.println(res3);
-        var res4 = dog - 3.5;
-        var res5 = cat - 1.6;
-        var res6 = paper - 7639;
-        System.out.println(res4);
-        System.out.println(res5);
-        System.out.println(res6);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
     }
 }
