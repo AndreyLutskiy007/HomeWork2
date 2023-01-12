0d771a919de9bca7bd44cac5cc8f1b6c2f4f0ea8
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-01-12 15:26:59 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var res1 = dog + 4;
-        var res2 = cat + 4;
-        var res3 = paper + 4;
-        System.out.println(res1);
-        System.out.println(res2);
-        System.out.println(res3);
-        var res4 = dog - 3.5;
-        var res5 = cat - 1.6;
-        var res6 = paper - 7639;
-        System.out.println(res4);
-        System.out.println(res5);
-        System.out.println(res6);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
     }
 }
